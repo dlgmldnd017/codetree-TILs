4505d317712c7e33a34626c1770ebfedaff95b77
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-06-08 10:05:04 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 100ms, Memory: 8MB, Status: Passed - CodeTree

--- 240608/괄호 문자열의 적합성 판단/parentheses-string.java
+++ 240608/괄호 문자열의 적합성 판단/parentheses-string.java
@@ -4,27 +4,29 @@ import java.io.*;
 public class Main {
     static String input = "";
     
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         input = br.readLine();
+
         System.out.println(solve());
     }
 
-    static String solve() {
+    static String solve(){
         Stack<Character> stack = new Stack<>();
         
-        for (int i = 0; i < input.length(); i++) {
-            char c = input.charAt(i);
-            if (c == '(') {
-                stack.push(c);
-            } else if (c == ')') {
-                if (stack.isEmpty()) {
-                    return "No";
-                }
-                stack.pop();
+        for(int i=0; i<input.length(); i++){
+            if(input.charAt(i) == '('){
+                stack.push('(');
+            }
+
+            else if(input.charAt(i) == ')'){
+                if(stack.isEmpty()) return "No";
+                else stack.pop();
             }
         }
 
-        return stack.isEmpty() ? "Yes" : "No";
+        if(stack.size() == 0) return "Yes";
+        else return "No";
     }
 }
