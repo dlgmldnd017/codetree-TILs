8658d4ee4b93d6e41a7acdb6a28f841906d245bf
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-06-08 10:01:00 +0900

    Title: 괄호 문자열의 적합성 판단, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240608/괄호 문자열의 적합성 판단/parentheses-string.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static String input = "";
+    
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        input = br.readLine();
+
+        System.out.println(solve());
+    }
+
+    static String solve(){
+        Stack<Character> stack = new Stack<>();
+        
+        for(int i=0; i<input.length(); i++){
+            if(input.charAt(i) == '('){
+                stack.push('(');
+            }
+
+            else if(input.charAt(i) == ')'){
+                if(stack.peek() == '(') stack.pop();
+                else return "No";
+            }
+        }
+
+        if(stack.size() == 0) return "Yes";
+        else return "No";
+    }
+}
