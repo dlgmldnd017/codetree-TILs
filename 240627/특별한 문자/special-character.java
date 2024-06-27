98b17d47f012e4a488493396b9398b516dc14080
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-06-27 10:49:43 +0900

    Title: 특별한 문자, Time: 221ms, Memory: 13MB, Status: Passed - CodeTree

--- 240627/특별한 문자/special-character.java
+++ 240627/특별한 문자/special-character.java
@@ -7,7 +7,7 @@ public class Main {
 
         String input = br.readLine();
 
-        HashMap<Character, Integer> map = new HashMap<>();
+        LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
 
         for(int i=0; i<input.length(); i++){
             char c = input.charAt(i);
