7e23f452bd2c6f323d81fd5a2681c945b20ea5e5
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-06-27 10:43:32 +0900

    Title: 특별한 문자, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240627/특별한 문자/special-character.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine();
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for(int i=0; i<input.length(); i++){
+            char c = input.charAt(i);
+            
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        char ans = ' ';
+        for(Map.Entry<Character, Integer> entry : map.entrySet()){
+            if(entry.getValue() == 1) ans = entry.getKey();
+        }
+
+        if(ans == ' ') System.out.println("None");
+        else System.out.println(ans);
+    }
+}
