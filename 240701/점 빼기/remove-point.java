0febbe26c74261d28b90bd8a8db771f6e49d67db
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-07-01 19:36:24 +0900

    Title: 점 빼기, Time: 938ms, Memory: 42MB, Status: Passed - CodeTree

---
+++ 240701/점 빼기/remove-point.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.io.*;
+
+class Point implements Comparable<Point>{
+    int x, y;
+
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Point p){
+        if(this.x != p.x) return this.x - p.x;
+        else return this.y - p.y;
+    }
+}
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        TreeSet<Point> set = new TreeSet<>();
+
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            set.add(new Point(x, y));
+        }
+
+        for(int i=0; i<m; i++){
+            int k = Integer.parseInt(br.readLine());
+
+            Point p = set.ceiling(new Point(k, 1));
+            
+            if(p != null) {
+                set.remove(p);
+                sb.append(p.x + " " + p.y + "\n");
+            }
+            else sb.append("-1 -1\n");
+        }
+        
+        System.out.println(sb);
+    }
+}
