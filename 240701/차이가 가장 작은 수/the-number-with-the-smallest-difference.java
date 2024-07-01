fd9d2c394696979a5c4df600cabde5f931a4552f
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-07-01 18:58:11 +0900

    Title: 차이가 가장 작은 수, Time: 174ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 240701/차이가 가장 작은 수/the-number-with-the-smallest-difference.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        TreeSet<Integer> set = new TreeSet<>();
+
+        for(int i=0; i<n; i++){
+            int x = Integer.parseInt(br.readLine());
+
+            set.add(x);
+        }
+
+        int x = set.last() - set.first();
+
+        if(x>=m) sb.append(x);
+        else sb.append(-1);
+
+        System.out.println(sb);
+    }
+}
