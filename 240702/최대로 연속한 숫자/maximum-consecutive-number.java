9dead10d4698b6bf8f51c09585a861e4f4521db8
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-07-02 09:35:46 +0900

    Title: 최대로 연속한 숫자, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240702/최대로 연속한 숫자/maximum-consecutive-number.java
+++ 240702/최대로 연속한 숫자/maximum-consecutive-number.java
@@ -1,44 +1,82 @@
 import java.util.*;
-import java.io.*;
 
 public class Main {
-    static StringBuilder sb = new StringBuilder();
+    static int[] parent;
+    static int[] size;
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        st = new StringTokenizer(br.readLine());
-
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-
-        TreeSet<Integer> set = new TreeSet<>();
-
-        for(int i=0; i<=n; i++){
-            set.add(i);
+        // 입력 받기
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int[] toRemove = new int[m];
+        for (int i = 0; i < m; i++) {
+            toRemove[i] = scanner.nextInt();
         }
 
-        st = new StringTokenizer(br.readLine());
+        // 초기화
+        parent = new int[n + 1];
+        size = new int[n + 1];
+        Arrays.fill(size, 1);
 
-        for(int i=0; i<m; i++){
-            set.remove(Integer.parseInt(st.nextToken()));
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
 
-            int maxLength = 1;
-            int cnt = 1;
-            Integer prev = null;
+        TreeSet<Integer> numbers = new TreeSet<>();
+        for (int i = 0; i <= n; i++) {
+            numbers.add(i);
+        }
 
-            for(Integer num : set){
-                if(prev != null && num == prev+1) cnt++;
-                else cnt = 1;
-
-                maxLength = Math.max(maxLength, cnt);
-                prev = num;
+        // 각 숫자를 제거하면서 최장 연속 수열 길이 계산
+        for (int i = 0; i < m; i++) {
+            numbers.remove(toRemove[i]);
+            if (numbers.isEmpty()) {
+                System.out.println(0);
+                continue;
             }
 
-            sb.append(maxLength+"\n");
+            // Union-Find를 사용하여 최장 연속 수열 길이 계산
+            for (int num : numbers) {
+                if (numbers.contains(num - 1)) {
+                    union(num - 1, num);
+                }
+                if (numbers.contains(num + 1)) {
+                    union(num, num + 1);
+                }
+            }
+
+            int maxLength = 0;
+            for (int num : numbers) {
+                maxLength = Math.max(maxLength, size[find(num)]);
+            }
+            System.out.println(maxLength);
+
+            // 제거된 숫자를 복원
+            for (int num : numbers) {
+                parent[num] = num;
+                size[num] = 1;
+            }
         }
-        
-        System.out.println(sb);
+
+        scanner.close();
+    }
+
+    public static int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    public static void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+
+        if (rootX != rootY) {
+            parent[rootY] = rootX;
+            size[rootX] += size[rootY];
+        }
     }
 }
