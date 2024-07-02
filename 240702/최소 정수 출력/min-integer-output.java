b2c2ad5bcd1aeaaa07b4030ea632c806b22dfb10
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-07-02 18:41:36 +0900

    Title: 최소 정수 출력, Time: 556ms, Memory: 35MB, Status: Passed - CodeTree

---
+++ 240702/최소 정수 출력/min-integer-output.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for(int i=0; i<n; i++){
+            int x = Integer.parseInt(br.readLine());
+            
+            if(x==0){
+                if(pq.size()==0) sb.append(0+"\n");
+                else sb.append(pq.poll()+"\n");
+            }
+            else{
+                pq.add(x);
+            }
+        }
+        
+        System.out.println(sb);
+    }
+}
