238d220da983d86c0f48b08ec80b95fbdfdb8880
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-07-03 15:15:58 +0900

    Title: 배열 추출 2, Time: 94ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240703/배열 추출 2/array-extraction-2.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Long> minus = new PriorityQueue<>();
+        PriorityQueue<Long> plus = new PriorityQueue<>();
+
+        for(int i=0; i<n; i++){
+            long x = Long.parseLong(br.readLine());
+
+            if(x==0){
+                if(minus.isEmpty() && plus.isEmpty()) sb.append("0\n");
+                else {
+                    long m = minus.isEmpty() ? 0 : minus.poll();
+                    long p = plus.isEmpty() ? 0 : plus.poll();
+
+                    if(m==0){
+                        sb.append(p + "\n");
+                    }
+
+                    else if(p==0){
+                        sb.append(m + "\n");
+                    }
+
+                    else{
+                        sb.append(-m+"\n");
+                        plus.add(p);
+                    }
+                }
+            }
+
+            else{
+                if(x>=0){
+                    plus.add(x);
+                }
+
+                else{
+                    minus.add(-x);
+                }
+            }
+        }
+        
+        System.out.println(sb);
+    }
+}
