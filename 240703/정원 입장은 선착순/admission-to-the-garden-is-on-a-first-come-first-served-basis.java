e263c6bc0663e8135c46553f63879472a880e06d
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-07-03 17:22:30 +0900

    Title: 정원 입장은 선착순, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240703/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.java
+++ 240703/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.java
@@ -1,33 +1,18 @@
 import java.util.*;
 import java.io.*;
 
-class Node1 implements Comparable<Node1>{
+class Node implements Comparable<Node>{
     int num, a, t;
 
-    public Node1(int num, int a, int t){
+    public Node(int num, int a, int t){
         this.num = num;
         this.a = a;
         this.t = t;
     }
 
     @Override
-    public int compareTo(Node1 n){
+    public int compareTo(Node n){
         if(this.a != n.a) return this.a - n.a;
-        return this.num - n.num;
-    }
-}
-
-class Node2 implements Comparable<Node2>{
-    int num, a, t;
-
-    public Node2(int num, int a, int t){
-        this.num = num;
-        this.a = a;
-        this.t = t;
-    }
-
-    @Override
-    public int compareTo(Node2 n){
         return this.num - n.num;
     }
 }
@@ -40,8 +25,8 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
         
-        PriorityQueue<Node1> pq = new PriorityQueue<>();
-        PriorityQueue<Node2> pq2 = new PriorityQueue<>();
+        PriorityQueue<Node> pq1 = new PriorityQueue<>();
+        PriorityQueue<Node> pq2 = new PriorityQueue<>();
 
         for(int i=1; i<=N; i++){
             st = new StringTokenizer(br.readLine());
@@ -49,39 +34,50 @@ public class Main {
             int a = Integer.parseInt(st.nextToken());
             int t = Integer.parseInt(st.nextToken());
 
-            pq.add(new Node1(i, a, t));
+            pq1.add(new Node(i, a, t));
         }
 
         int ans = Integer.MIN_VALUE;
-        int curTime = 0;
+        int curTime = pq1.peek().a;
 
-        while(!pq.isEmpty()){
-            Node1 n1 = pq.poll();
+        while(!pq1.isEmpty()){
 
-            while(!pq.isEmpty() && pq.peek().a <= curTime){
-                Node1 n = pq.poll();
-
-                pq2.add(new Node2(n.num, n.a, n.t));
+            // 먼저, curTime 아래에 도착한 사람들을 뽑는다.
+            while(!pq1.isEmpty() && pq1.peek().a <= curTime){
+                pq2.add(pq1.poll());
             }
 
-            if(pq2.size()!=0){
-                pq2.add(new Node2(n1.num, n1.a, n1.t));
+            if(!pq1.isEmpty() && pq2.size()==0){
+                pq2.add(pq1.poll());
+            }
 
-                Node2 cur = pq2.poll();
-                ans = Math.max(ans, curTime-cur.a);
-                
-                curTime += cur.t;
+            if(pq2.size()==1){
+                Node n = pq2.poll();
 
-                while(!pq2.isEmpty()){
-                    Node2 n = pq2.poll();
-                    pq.add(new Node1(n.num, n.a, n.t));
-                }
+                ans = Math.max(ans, curTime-n.a);
+                if(curTime - n.a > 0) curTime = n.a + n.t;
+                else curTime += n.t;
             }
 
             else{
-                ans = Math.max(ans, curTime-n1.a);
-                curTime = n1.a + n1.t;
-                if(ans<0) ans = 0;
+                while(pq2.size() > 1){
+                    Node n1 = pq2.poll();
+                    Node n2 = pq2.poll();
+
+                    if(n1.num > n2.num){
+                        pq1.add(n1);
+                        pq2.add(n2);
+                    }
+
+                    else{
+                        pq1.add(n2);
+                        pq2.add(n1);
+                    }
+                }
+
+                Node cur = pq2.poll();
+                ans = Math.max(ans, curTime-cur.a);
+                curTime += cur.t;
             }
         }
 
