a895862e5b33ced15836f1753e1a496223dcedb6
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-07-03 10:55:15 +0900

    Title: 중앙값, Time: 682ms, Memory: 42MB, Status: Passed - CodeTree

--- 240703/중앙값/median.java
+++ 240703/중앙값/median.java
@@ -1,58 +1,41 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    private PriorityQueue<Integer> maxHeap; // 최대 힙
-    private PriorityQueue<Integer> minHeap; // 최소 힙
+    static StringBuilder sb = new StringBuilder();
 
-    public Main() {
-        // 최대 힙은 역순으로 정렬하여 최대값을 루트에 위치시킴
-        maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-        // 최소 힙은 기본 정렬(오름차순)
-        minHeap = new PriorityQueue<>();
-    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-    public void addNum(int num) {
-        // 최대 힙에 새 숫자를 추가
-        maxHeap.offer(num);
-        // 최대 힙의 루트를 최소 힙으로 이동
-        minHeap.offer(maxHeap.poll());
+        int t = Integer.parseInt(br.readLine());
 
-        // 최소 힙의 크기가 최대 힙보다 크면, 최소 힙의 루트를 최대 힙으로 이동
-        if (minHeap.size() > maxHeap.size()) {
-            maxHeap.offer(minHeap.poll());
-        }
-    }
+        for(int T=1; T<=t; T++){
+            int m = Integer.parseInt(br.readLine());
 
-    public int findMedian() {
-        return maxHeap.peek();
-    }
+            PriorityQueue<Integer> higher = new PriorityQueue<>(); // higher.peek()를 통해 중앙값 출력 (내림차순)
+            PriorityQueue<Integer> lower = new PriorityQueue<>(Collections.reverseOrder()); // (오름차순)
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
 
-        for (int i = 0; i < t; i++) {
-            int m = sc.nextInt();
-            Main mf = new Main();
-            List<Integer> medians = new ArrayList<>();
+            higher.add(Integer.parseInt(st.nextToken()));
+            sb.append(higher.peek()+" ");
 
-            for (int j = 0; j < m; j++) {
-                int num = sc.nextInt();
-                mf.addNum(num);
+            for(int i=1; i<m; i++){
+                int x = Integer.parseInt(st.nextToken());
 
-                if (j % 2 == 0) { // 홀수 번째 수 (0-indexed에서 짝수)
-                    medians.add(mf.findMedian());
-                }
+                if(x > higher.peek()) higher.add(x);
+                else lower.add(x);
+
+                if(higher.size()-lower.size()>1) lower.add(higher.poll());
+                else if(lower.size() > higher.size()) higher.add(lower.poll());
+
+                if(i%2==0) sb.append(higher.peek()+" ");
             }
 
-            // 중앙값 출력
-            for (int k = 0; k < medians.size(); k++) {
-                if (k > 0) System.out.print(" ");
-                System.out.print(medians.get(k));
-            }
-            System.out.println();
+            sb.append("\n");
         }
-
-        sc.close();
+        
+        System.out.println(sb);
     }
 }
