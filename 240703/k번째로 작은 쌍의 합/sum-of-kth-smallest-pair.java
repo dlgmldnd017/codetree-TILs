87833ccb031c3254d09da1faf4083d673cb25b2f
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-07-03 15:00:43 +0900

    Title: k번째로 작은 쌍의 합, Time: 577ms, Memory: 27MB, Status: Passed - CodeTree

--- 240703/k번째로 작은 쌍의 합/sum-of-kth-smallest-pair.java
+++ 240703/k번째로 작은 쌍의 합/sum-of-kth-smallest-pair.java
@@ -1,61 +1,66 @@
 import java.util.*;
+import java.io.*;
+
+class Point implements Comparable<Point>{
+    int x, y, len;
+
+    public Point(int x, int y, int len){
+        this.x = x;
+        this.y = y;
+        this.len = len;
+    }
+
+    @Override
+    public int compareTo(Point p){
+        if(this.len != p.len) return this.len - p.len;
+        else if(this.x != p.x) return this.x - p.x;
+        else return this.y - p.y;
+    }
+}
 
 public class Main {
-    static class Pair implements Comparable<Pair> {
-        int sum;
-        int i, j;
-        
-        Pair(int sum, int i, int j) {
-            this.sum = sum;
-            this.i = i;
-            this.j = j;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken())+1;
+        int m = Integer.parseInt(st.nextToken())+1;
+        int k = Integer.parseInt(st.nextToken());
+
+        int arr1[] = new int[n];
+        int arr2[] = new int[m];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=1; i<n; i++){
+            arr1[i] = Integer.parseInt(st.nextToken());
         }
-        
-        public int compareTo(Pair other) {
-            return this.sum - other.sum;
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=1; i<m; i++){
+            arr2[i] = Integer.parseInt(st.nextToken());
         }
-    }
-    
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int k = sc.nextInt();
-        
-        int[] arr1 = new int[n];
-        int[] arr2 = new int[m];
-        
-        for (int i = 0; i < n; i++) {
-            arr1[i] = sc.nextInt();
-        }
-        
-        for (int i = 0; i < m; i++) {
-            arr2[i] = sc.nextInt();
-        }
-        
+
         Arrays.sort(arr1);
         Arrays.sort(arr2);
-        
-        PriorityQueue<Pair> minHeap = new PriorityQueue<>();
-        
-        for (int i = 0; i < n; i++) {
-            minHeap.offer(new Pair(arr1[i] + arr2[0], i, 0));
+
+        PriorityQueue<Point> pq = new PriorityQueue<>();
+
+        for(int i=1; i<n; i++){
+            pq.add(new Point(i,1,arr1[i]+arr2[1]));
+        }
+
+        for(int i=0; i<k-1; i++){
+            Point p = pq.poll();
+
+            int x = p.x;
+            int y = p.y+1;
+
+            if(y < m) pq.add(new Point(x, y, arr1[x] + arr2[y]));
         }
         
-        Pair current = null;
-        for (int count = 0; count < k; count++) {
-            current = minHeap.poll();
-            int i = current.i;
-            int j = current.j;
-            
-            if (j + 1 < m) {
-                minHeap.offer(new Pair(arr1[i] + arr2[j + 1], i, j + 1));
-            }
-        }
-        
-        System.out.println(current.sum);
-        
-        sc.close();
+        System.out.println(pq.peek().len);
     }
 }
