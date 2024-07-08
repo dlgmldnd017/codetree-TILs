8d69e3a671e782ec3ba744b92a6b561e3b52600a
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-07-08 19:01:29 +0900

    Title: 정수 n개의 합 2, Time: 249ms, Memory: 20MB, Status: Passed - CodeTree

--- 240708/정수 n개의 합 2/sum-of-n-integers-2.java
+++ 240708/정수 n개의 합 2/sum-of-n-integers-2.java
@@ -2,7 +2,6 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
@@ -12,19 +11,25 @@ public class Main {
         int k = Integer.parseInt(st.nextToken());
 
         int arr[] = new int[n];
+        int p[] = new int[n];
 
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<n; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
+        
+        p[0] = arr[0];
+
+        for(int i=1; i<n; i++){
+            p[i] = arr[i] + p[i-1];
+        }
 
         int max = Integer.MIN_VALUE;
 
-        for(int i=1; i<n-1; i++){
-            int x1 = arr[i] + arr[i-1];
-            int x2 = arr[i] + arr[i+1];
+        for(int i=0; i<n-k; i++){
+            int x = p[k-1+i] - p[i] + arr[i];
 
-            max = Math.max(max, Math.max(x1, x2));
+            max = Math.max(max, x);
         }
         
         System.out.println(max);
