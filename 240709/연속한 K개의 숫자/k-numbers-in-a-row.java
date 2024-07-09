75094bc0e08f53392b8220b2426b01cb621937a7
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-07-09 11:21:19 +0900

    Title: 연속한 K개의 숫자, Time: 114ms, Memory: 10MB, Status: Passed - CodeTree

--- 240709/연속한 K개의 숫자/k-numbers-in-a-row.java
+++ 240709/연속한 K개의 숫자/k-numbers-in-a-row.java
@@ -1,36 +1,32 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        int N = sc.nextInt();
-        int K = sc.nextInt();
-        int B = sc.nextInt();
-        
-        // 고장난 신호등 위치를 저장할 배열
-        boolean[] broken = new boolean[N + 1];
-        
-        for (int i = 0; i < B; i++) {
-            int brokenPosition = sc.nextInt();
-            broken[brokenPosition] = true;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken())+1;
+        int K = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        boolean exist[] = new boolean[N];
+        for(int i=0; i<B; i++){
+            exist[Integer.parseInt(br.readLine())] = true;
+        }
+
+        int prefixSum[] = new int[N];
+        for(int i=1; i<N; i++){
+            prefixSum[i] = prefixSum[i-1] + (exist[i] ? 1:0);
+        }
+
+        int min = Integer.MAX_VALUE;
+        for(int i=K; i<N; i++){
+            min = Math.min(min, prefixSum[i] - prefixSum[i-K]);
         }
         
-        // 누적합 배열
-        int[] prefixSum = new int[N + 1];
-        
-        // 누적합 배열을 채운다
-        for (int i = 1; i <= N; i++) {
-            prefixSum[i] = prefixSum[i - 1] + (broken[i] ? 1 : 0);
-        }
-        
-        // 최소로 고쳐야 할 신호등 수를 찾는다
-        int minToFix = Integer.MAX_VALUE;
-        for (int i = K; i <= N; i++) {
-            int brokenInWindow = prefixSum[i] - prefixSum[i - K];
-            minToFix = Math.min(minToFix, brokenInWindow);
-        }
-        
-        System.out.println(minToFix);
+        System.out.println(min);
     }
 }
