30cc09cc7733455f66761214849c262070a04fc9
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-07-13 21:52:16 +0900

    Title: 행복한 수열의 개수, Time: 227ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 240713/행복한 수열의 개수/number-of-happy-sequence.java
@@ -0,0 +1,64 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int n, m, ans;
+    static int arr[][];
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n][n];
+
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+
+            for(int j=0; j<n; j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        solve();
+
+        System.out.println(ans);
+    }
+
+    static void solve(){
+        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
+
+        for(int i=0; i<n; i++){
+            
+            int cnt = 1;
+            int prev = arr[i][0];
+        
+            // 행 확인
+            for(int j=1; j<n; j++){
+                if(arr[i][j] == prev) cnt++;
+                else cnt=1;
+
+                if(cnt==m) {
+                    ans++;
+                    break;
+                }    
+            }
+            
+            cnt = 1;
+            prev = arr[0][i];
+            // 열 확인
+            for(int j=1; j<n; j++){
+                if(arr[j][i] == prev) cnt++;
+                else cnt=1;
+
+                if(cnt==m) {
+                    ans++;
+                    break;
+                }    
+            }
+        }
+    }
+}
