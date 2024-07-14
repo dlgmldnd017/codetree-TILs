46dbd2c4283779041e01fa4607b4395c8213575d
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-07-14 19:52:27 +0900

    Title: 점 개수 세기 2, Time: 5072ms, Memory: 35MB, Status: Time Limit Exceed - CodeTree

--- 240714/점 개수 세기 2/count-number-of-points-2.java
+++ 240714/점 개수 세기 2/count-number-of-points-2.java
@@ -1,12 +1,16 @@
 import java.util.*;
 import java.io.*;
 
-class Point{
+class Point implements Comparable<Point>{
     int x, y;
 
     public Point(int x, int y){
         this.x = x;
         this.y = y;
+    }
+
+    public int compareTo(Point p){
+        return this.x - p.x;
     }
 }
 
@@ -14,12 +18,8 @@ public class Main {
     static StringBuilder sb = new StringBuilder();
 
     static int N, Q;
-    static int preifxSum[][];
-
-    static Point p[];
-
-    static TreeSet<Integer> set = new TreeSet<>();
-    static TreeMap<Integer, Integer> map = new TreeMap<>();
+    
+    static TreeSet<Point> set = new TreeSet<>();
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -29,37 +29,12 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         Q = Integer.parseInt(st.nextToken());
 
-        p = new Point[N];
-
         for(int i=0; i<N; i++){
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
 
-            set.add(x);
-            set.add(y);
-
-            p[i] = new Point(x, y);
-        }
-
-        int cnt=1;
-        for(Integer i : set){
-            map.put(i, cnt++);
-        }
-
-        preifxSum = new int[5002][5002];
-
-        for(Point i : p){
-            int x = map.get(i.x);
-            int y = map.get(i.y);
-
-            preifxSum[x][y]++;
-        }
-
-        for(int i=1; i<=cnt; i++){
-            for(int j=1; j<=cnt; j++){
-                preifxSum[i][j] += preifxSum[i][j-1] + preifxSum[i-1][j] - preifxSum[i-1][j-1];
-            }
+            set.add(new Point(x, y));
         }
 
         for(int q=0; q<Q; q++){
@@ -69,36 +44,16 @@ public class Main {
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
 
-            x1 = getCeilingValue(x1);
-            y1 = getCeilingValue(y1);
-            x2 = getFlooringValue(x2);
-            y2 = getFlooringValue(y2);
+            NavigableSet<Point> ns = set.subSet(new Point(x1, y1), true, new Point(x2, y2), true);
 
-            sb.append(getSum(x1, y1, x2, y2)+"\n");
+            int cnt=0;
+            for(Point p : ns){
+                if(p.y>=y1 && p.y<=y2) cnt++;
+            }
+
+            sb.append(cnt+"\n");
         }
 
         System.out.println(sb);
-    }
-
-    static int getCeilingValue(int x){
-        if(set.ceiling(x) != null){
-            int newX = set.ceiling(x);
-            return map.get(newX);
-        } 
-        
-        return set.size()+1;
-    }
-
-    static int getFlooringValue(int x){
-        if(set.floor(x) != null) {
-            int newX = set.floor(x);
-            return map.get(newX);
-        }
-
-        return 0;
-    }
-
-    static int getSum(int x1, int y1, int x2, int y2){
-        return preifxSum[x2][y2] - preifxSum[x1-1][y2] - preifxSum[x2][y1-1] + preifxSum[x1-1][y1-1];
     }
 }
