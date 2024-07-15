59e3c15553d5abb6749ec1ae93d12111e2516ade
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-07-15 15:16:28 +0900

    Title: 씨 오 더블유, Time: 88ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240715/씨 오 더블유/c-o-w.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N, L[], R[], ans;
+    static String input;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+
+        input = br.readLine();
+
+        solve();
+
+        System.out.println(ans);
+    }
+
+    static void solve(){
+        L = new int[N];
+        if(input.charAt(0)=='C') L[0] = 1;
+
+        for(int i=1; i<N; i++){
+            L[i] = L[i-1];
+
+            if(input.charAt(i)=='C') L[i] += 1;
+        }
+
+        R = new int[N];
+        if(input.charAt(N-1)=='W') R[N-1] = 1;
+
+        for(int i=N-2; i>=0; i--){
+            R[i] = R[i+1];
+
+            if(input.charAt(i)=='W') R[i] += 1;
+        }
+
+        for(int i=1; i<N-1; i++){
+            if(input.charAt(i)=='O') ans += (L[i]*R[i]);
+        }
+    }
+}
