dfbcedc5ac82d8862c42fc4edc18f32f04e96697
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-07-29 11:51:37 +0900

    Title: 겹치는 선분들, Time: 1567ms, Memory: 58MB, Status: Passed - CodeTree

--- 240729/겹치는 선분들/overlapping-lines.java
+++ 240729/겹치는 선분들/overlapping-lines.java
@@ -1,69 +1,56 @@
 import java.util.*;
-import java.io.*;
-
-class Point implements Comparable<Point>{
-    int x, v;
-
-    public Point(int x, int v){
-        this.x = x;
-        this.v = v;
-    }
-
-    public int compareTo(Point p){
-        return this.x - p.x;
-    }
-}
 
 public class Main {
-    static int N, K, ans;
-    static List<Point> list = new ArrayList<>();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt(); // 선분의 개수
+        int K = scanner.nextInt(); // 최소 겹침 수
+        List<Event> events = new ArrayList<>();
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        int currentPos = 0;
+        for (int i = 0; i < N; i++) {
+            int length = scanner.nextInt();
+            String direction = scanner.next();
+            int start = currentPos;
+            int end = (direction.equals("R")) ? currentPos + length : currentPos - length;
 
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-
-
-        int cur = 0;
-        for(int i=0; i<N; i++){
-            st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            String command = st.nextToken();
-
-            if(command.equals("R")){
-                list.add(new Point(cur, +1));
-                list.add(new Point(cur+x, -1));
-
-                cur += x;
+            // 각 선분의 시작과 끝 기록 (시작을 1로, 끝을 -1로)
+            if (start < end) {
+                events.add(new Event(start, 1));
+                events.add(new Event(end, -1));
+            } else {
+                events.add(new Event(end, 1));
+                events.add(new Event(start, -1));
             }
-            else{
-                list.add(new Point(cur-x, +1));
-                list.add(new Point(cur, -1));
-
-                cur -= x;
-            }
+            currentPos = end;
         }
 
-        Collections.sort(list);
+        // 이벤트를 위치 순서대로 정렬
+        Collections.sort(events, (a, b) -> (a.position != b.position) ? a.position - b.position : a.type - b.type);
 
-        solve();
+        int overlapCount = 0;
+        int previousPos = Integer.MIN_VALUE;
+        int result = 0;
 
-        System.out.println(ans);
+        // 스위핑을 통해 겹치는 구간 계산
+        for (Event event : events) {
+            if (overlapCount >= K) {
+                result += event.position - previousPos;
+            }
+            overlapCount += event.type;
+            previousPos = event.position;
+        }
+
+        System.out.println(result);
     }
 
-    static void solve(){
-        int cnt=0, cur=0;
+    static class Event {
+        int position;
+        int type; // 시작은 1, 끝은 -1
 
-        for(Point p : list){
-            if(cnt>=2){
-                ans += p.x - cur;
-            }
-
-            cur = p.x;
-            cnt += p.v;
+        Event(int position, int type) {
+            this.position = position;
+            this.type = type;
         }
     }
 }
