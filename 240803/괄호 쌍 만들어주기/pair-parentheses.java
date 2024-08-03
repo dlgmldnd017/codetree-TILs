e19a743afb2256948d84e1aac2767c473dd38ba1
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-08-03 22:05:45 +0900

    Title: 괄호 쌍 만들어주기, Time: 217ms, Memory: 13MB, Status: Passed - CodeTree

--- 240803/괄호 쌍 만들어주기/pair-parentheses.java
+++ 240803/괄호 쌍 만들어주기/pair-parentheses.java
@@ -1,44 +1,43 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
-public class Main {
+public class Main {    
+    public static final int MAX_N = 100000;
+    
+    // 변수 선언
+    public static int n;
+    public static String str;
+    public static int[] R = new int[MAX_N];
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-
-        List<Integer> openPositions = new ArrayList<>();
-        List<Integer> closePositions = new ArrayList<>();
-
-        // 연속된 여는 괄호와 닫는 괄호의 위치 저장
-        for (int i = 0; i < input.length() - 1; i++) {
-            if (input.charAt(i) == '(' && input.charAt(i + 1) == '(') {
-                openPositions.add(i);
-            } else if (input.charAt(i) == ')' && input.charAt(i + 1) == ')') {
-                closePositions.add(i);
-            }
+        Scanner sc = new Scanner(System.in);
+        // 입력:
+        str = sc.next();
+        n = str.length();
+        
+        // R 배열을 채워줍니다.
+        // Ri = i번지부터 N - 1번지 까지 있는 문자열중에 
+        //      연속하여 ))가 나오는 횟수
+        R[n - 1] = 0;
+        for(int i = n - 2; i >= 0; i--) {
+            // i, i + 1로 )) 조합이 만들어진다면
+            // i번부터 만들 수 있는 조합 수가 1만큼 더 늘어납니다.
+            if(str.charAt(i) == ')' && str.charAt(i + 1) == ')')
+                R[i] = R[i + 1] + 1;
+            // 그렇지 않다면, i + 1번부터 만들 수 있는 조합 수와 같습니다.
+            else
+                R[i] = R[i + 1];
         }
-
-        int openSize = openPositions.size();
-        int closeSize = closePositions.size();
-
-        // 전처리 작업: 각 위치에서 이후에 나온 닫는 괄호의 수를 저장
-        int[] closeCountFromEnd = new int[input.length() + 1];
-        for (int i = closeSize - 1; i >= 0; i--) {
-            closeCountFromEnd[closePositions.get(i)] = 1;
+        
+        // 답을 구해줍니다.
+        // 현재 i, i + 1 조합과
+        // 쌍을 이루는 서로 다른 가짓수를 세어
+        // 답에 더해줍니다.
+        long ans = 0;
+        for(int i = 0; i < n - 2; i++) {
+            if(str.charAt(i) == '(' && str.charAt(i + 1) == '(')
+                ans += R[i + 2];
         }
-        for (int i = input.length() - 2; i >= 0; i--) {
-            closeCountFromEnd[i] += closeCountFromEnd[i + 1];
-        }
-
-        int result = 0;
-        // 가능한 쌍 찾기: 여는 괄호 위치에서 이후의 닫는 괄호의 수를 더함
-        for (int openPos : openPositions) {
-            if (openPos + 2 < input.length()) {
-                result += closeCountFromEnd[openPos + 2];
-            }
-        }
-
-        System.out.println(result);
+        
+        System.out.print(ans);
     }
 }
