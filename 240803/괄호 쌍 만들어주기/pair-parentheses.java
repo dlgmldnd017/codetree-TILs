104f56b9e8c4608697fe3b427f126cb4f41addad
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-08-03 22:05:31 +0900

    Title: 괄호 쌍 만들어주기, Time: 290ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 240803/괄호 쌍 만들어주기/pair-parentheses.java
+++ 240803/괄호 쌍 만들어주기/pair-parentheses.java
@@ -6,10 +6,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        
+
         List<Integer> openPositions = new ArrayList<>();
         List<Integer> closePositions = new ArrayList<>();
-        
+
         // 연속된 여는 괄호와 닫는 괄호의 위치 저장
         for (int i = 0; i < input.length() - 1; i++) {
             if (input.charAt(i) == '(' && input.charAt(i + 1) == '(') {
@@ -18,25 +18,27 @@ public class Main {
                 closePositions.add(i);
             }
         }
-        
-        // 전처리 작업: 각 위치에서 이전에 나온 닫는 괄호의 수를 저장
-        int[] closeCount = new int[input.length()];
-        int count = 0;
-        for (int i = 0; i < input.length(); i++) {
-            if (i > 0) {
-                closeCount[i] = closeCount[i - 1];
-            }
-            if (i < input.length() - 1 && input.charAt(i) == ')' && input.charAt(i + 1) == ')') {
-                closeCount[i]++;
-            }
+
+        int openSize = openPositions.size();
+        int closeSize = closePositions.size();
+
+        // 전처리 작업: 각 위치에서 이후에 나온 닫는 괄호의 수를 저장
+        int[] closeCountFromEnd = new int[input.length() + 1];
+        for (int i = closeSize - 1; i >= 0; i--) {
+            closeCountFromEnd[closePositions.get(i)] = 1;
         }
-        
+        for (int i = input.length() - 2; i >= 0; i--) {
+            closeCountFromEnd[i] += closeCountFromEnd[i + 1];
+        }
+
         int result = 0;
         // 가능한 쌍 찾기: 여는 괄호 위치에서 이후의 닫는 괄호의 수를 더함
         for (int openPos : openPositions) {
-            result += closeCount[input.length() - 1] - closeCount[openPos + 1];
+            if (openPos + 2 < input.length()) {
+                result += closeCountFromEnd[openPos + 2];
+            }
         }
-        
+
         System.out.println(result);
     }
 }
