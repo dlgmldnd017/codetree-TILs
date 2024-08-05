f7ab95bf2f679a700b81344aa2d86a5e836336d3
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-08-05 10:47:54 +0900

    Title: 최소 에너지 비용, Time: 477ms, Memory: 24MB, Status: Passed - CodeTree

--- 240805/최소 에너지 비용/minimum-energy-cost.java
+++ 240805/최소 에너지 비용/minimum-energy-cost.java
@@ -1,35 +1,39 @@
 import java.util.Scanner;
 
 public class Main {
+    public static final int MAX_N = 100000;
+    
+    // 변수 선언
+    public static int n;
+    public static int[] dist = new int[MAX_N + 1];
+    public static int[] cost = new int[MAX_N + 1];
+    public static int[] minCost = new int[MAX_N + 1];
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // 입력:
+        n = sc.nextInt();
+        for(int i = 2; i <= n; i++)
+            dist[i] = sc.nextInt();
+        for(int i = 1; i <= n; i++)
+            cost[i] = sc.nextInt();
         
-        // 장소의 개수
-        int n = sc.nextInt();
+        // i번째 장소로 이제 출발할 때 기준으로
+        // 지금까지 왔던 장소 중
+        // 에너지 1을 채우는데 필요한 비용이
+        // 가장 작은 곳을 전처리로 저장합니다.
+        minCost[2] = cost[1];
+        for(int i = 3; i <= n; i++)
+            minCost[i] = Math.min(minCost[i - 1], cost[i - 1]);
         
-        // 장소와 장소 사이의 이동 에너지
-        int[] moveEnergy = new int[n - 1];
-        for (int i = 0; i < n - 1; i++) {
-            moveEnergy[i] = sc.nextInt();
+        long ans = 0;
+        for(int i = 1; i <= n; i++) {
+            // 지금까지 왔던 장소 중 에너지 1을 채우는데 필요한 비용이
+            // 가장 작은 곳에서 그만큼의 에너지를 충전합니다.
+            ans += (long) minCost[i] * dist[i];
         }
         
-        // 각 장소마다 에너지 1을 채우는데 드는 비용
-        int[] fillCost = new int[n];
-        for (int i = 0; i < n; i++) {
-            fillCost[i] = sc.nextInt();
-        }
-        
-        // 최소 비용 계산
-        long minCost = 0;
-        long currentMinCost = fillCost[0];
-        
-        for (int i = 0; i < n - 1; i++) {
-            minCost += currentMinCost * moveEnergy[i];
-            if (fillCost[i + 1] < currentMinCost) {
-                currentMinCost = fillCost[i + 1];
-            }
-        }
-        
-        System.out.println(minCost);
+        // 정답을 출력합니다.
+        System.out.print(ans);
     }
 }
