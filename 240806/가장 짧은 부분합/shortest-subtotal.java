c0383d59ce2c71d671ba8284cfcb2b9984cd2f98
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-08-06 11:13:07 +0900

    Title: 가장 짧은 부분합, Time: 313ms, Memory: 19MB, Status: Passed - CodeTree

---
+++ 240806/가장 짧은 부분합/shortest-subtotal.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N, S, Arr[], ans;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+
+        Arr = new int[N+1];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=1; i<=N; i++){
+            Arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        ans = Integer.MAX_VALUE;
+
+        solve();
+
+        if(ans == Integer.MAX_VALUE) ans = -1;
+        System.out.println(ans);
+    }
+
+    static void solve(){
+        long sum = 0;
+        int j = 0;
+
+        for(int i=1; i<=N; i++){
+
+            while(j+1<=N && sum+Arr[j+1]<S){
+                sum += Arr[j+1];
+                j++;
+            }
+
+            if(j+1<=N && sum<S){
+                sum += Arr[j+1];
+                j++;
+            }
+
+            if(sum>=S) ans = Math.min(ans, j-i+1);
+
+            sum -= Arr[i];
+        }
+    }
+}
