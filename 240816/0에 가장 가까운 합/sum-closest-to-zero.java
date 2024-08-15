5f2ab8769ff6b998522b95f2a8e0d4b6de53eb52
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-08-16 01:03:36 +0900

    Title: 0에 가장 가까운 합, Time: 376ms, Memory: 23MB, Status: Passed - CodeTree

--- 240816/0에 가장 가까운 합/sum-closest-to-zero.java
+++ 240816/0에 가장 가까운 합/sum-closest-to-zero.java
@@ -32,7 +32,7 @@ public class Main {
         for(int i=1; i<=N; i++){
             if(i<j) ans = Math.min(ans, (Math.abs(A[i]+A[j])));
 
-            while(i<j-1 && (A[i] + A[j-1]) > 0){
+            while(i<j-1 && (A[i] + A[j]) > 0){
                 j--;
                 ans = Math.min(ans, (Math.abs(A[i]+A[j])));
             }
