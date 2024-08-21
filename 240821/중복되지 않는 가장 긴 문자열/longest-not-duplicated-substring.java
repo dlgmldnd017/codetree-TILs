b075f6fe7343a63fc8efc6f9443ce5daa97ac2b6
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-08-21 11:49:33 +0900

    Title: 중복되지 않는 가장 긴 문자열, Time: 197ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240821/중복되지 않는 가장 긴 문자열/longest-not-duplicated-substring.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N, ans;
+    static String str;
+    static Map<Character, Integer> map = new HashMap<>();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        str = br.readLine();
+
+        N = str.length();
+
+        solve();
+
+        System.out.println(ans);
+    }
+
+    static void solve(){
+		int i=0;
+
+        for(int j=0; j<N; j++){
+            char c = str.charAt(j);
+
+            if(map.containsKey(c)) i = Math.max(i, map.get(c)+1);
+
+            map.put(c, j);
+
+            ans = Math.max(ans, j-i+1);
+        }
+    }
+}
