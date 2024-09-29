e6c4f44e5e019bea3366dacbcc26df8f0c57e3d0
HuiUng Lee <42001306+dlgmldnd017@users.noreply.github.com>
2024-09-29 15:16:25 +0900

    Title: 바이러스 검사, Time: 532ms, Memory: 73MB, Status: Passed - CodeTree

--- 240929/바이러스 검사/virus-detector.java
+++ 240929/바이러스 검사/virus-detector.java
@@ -3,7 +3,8 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, restCnt[], LDRMax, MBRMax, ans;
+    static int N, restCnt[], LDRMax, MBRMax;
+    static long ans;
 
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
